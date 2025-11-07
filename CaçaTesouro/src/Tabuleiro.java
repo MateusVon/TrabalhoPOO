8c99e8dadbc1e4f66a975c280d52be97d2542c04
Eduardo Coelho <eduardohrcoelhomkt@gmail.com>
2025-11-07 15:51:41 -0300

    Implementando a Classe Tabuleiro

--- CaçaTesouro/src/Tabuleiro.java
+++ CaçaTesouro/src/Tabuleiro.java
@@ -1,3 +1,16 @@
+import java.util.*;
+
 public class Tabuleiro {
+    private Tesouro [][] mapaTesouros;
+
+    public Tabuleiro(){
+        mapaTesouros = new Tesouro[10][10];
+    }
+
+    public void posicionarTesouro(int linha, int coluna, Tesouro t){
+
+    }
+    
+    
 
 }
