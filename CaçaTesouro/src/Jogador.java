be7f0e39425115546b7a7e944dbddb44bb94c47e
Eduardo Coelho <eduardohrcoelhomkt@gmail.com>
2025-11-13 09:58:47 -0300

    Limpeza de metodos não utilizados na classe Jogador

--- CaçaTesouro/src/Jogador.java
+++ CaçaTesouro/src/Jogador.java
@@ -33,22 +33,13 @@ public class Jogador {
     return this.nome;
   }
 
-  public void setPontuacao(int pontuacao) {
-    this.pontuacao = pontuacao;
-  }
-
   public double getPontuacao() {
     return this.pontuacao;
-  }
-
-  public void setMeuTabuleiro(Tabuleiro meuTabuleiro) {
-    this.meuTabuleiro = meuTabuleiro;
   }
 
   public Tabuleiro getMeuTabuleiro() {
     return this.meuTabuleiro;
   }
-
 
   // Verifica se o jogador ja tentou "cavar" nessa coordenada,
   // usando o ArrayList 'jogadasFeitas'
@@ -99,39 +90,6 @@ public class Jogador {
     boolean sucesso = this.meuTabuleiro.posicionarTesouro(linha, coluna, novoTesouro);
     return sucesso;
   }
-  
-  // Posiciona o tesouro automaticamente
-  
-  /*public void posicionarTesouro() {
-    System.out.println("---" + this.nome + ", posicione seus tesouros! ---");
-    System.out.println("(O tabuleiro é 10x1, use linha e colunas de 0 a 9)");
-
-    loopPosicionamento("verde", 3);
-    loopPosicionamento("amarelo", 3);
-    loopPosicionamento("vermelho", 2);
-
-    System.out.println("---" + this.nome + " terminou de posicionar! ---");
-  }
-
-  private void loopPosicionamento(String cor, int quantidade) {
-    Random aleatorio = new Random();
-
-    for (int i = 0; i < quantidade; i++) {
-      boolean sucesso = false;
-
-      while (!sucesso) {
-        int linha = aleatorio.nextInt(10);
-        int coluna = aleatorio.nextInt(10);
-
-        sucesso = this.posicionarTesouro(linha, coluna, cor);
-      }
-    }
-
-  }
-
-  public boolean posicionarTesouro(int linha, int coluna) {
-    return this.posicionarTesouro(linha, coluna, "Amarelo");
-  }*/
 
   public void adicionarPontos(double pontosGanhos) {
     this.pontuacao = this.pontuacao + pontosGanhos;
